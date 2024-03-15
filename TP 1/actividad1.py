cc529aaa3edd7b89ccc182519aedc911c5ce6581
Joaquin Ruiz <joaquin.ruiz2012@gmail.com>
2024-03-15 16:20:02 -0300

    TP 1 - Ejercicios opcionales

--- TP 1/actividad1.py
+++ TP 1/actividad1.py
@@ -1,18 +1,36 @@
 import tramasService
 import utils
 
-text = utils.leerArchivo('/home/joaco/Escritorio/Redes de Computadoras/TPS/TP 1/Tramas_802-15-4.log')
+text = utils.leerArchivo('./TP 1/Tramas_802-15-4.log')
 
 tramas = tramasService.separarTramas(text)
 
 print("En total hay", len(tramas), "tramas")
 
-tramasFiltradas = tramasService.contarTramasCorrectas(tramas)
+tramasFiltradas = tramasService.contarTramasLongitudCorrecta(tramas)
 
 print("En total hay", len(tramasFiltradas[0]), "tramas con correcta longitud")
 
-tramasCorrectas = tramasService.verificarTramas(tramasFiltradas[0])[0]
+tramasCorrectas = tramasService.contarTramasChecksumCorrecto(tramasFiltradas[0])[0]
 
 print("En total hay", len(tramasCorrectas), "tramas con suma de verificación correcta y", (len(tramasFiltradas[0]) - len(tramasCorrectas)), "tramas con suma de verificación incorrecta.")
 
 print("Hay", tramasService.contarSecuenciasDeEscape(tramas), "tramas que utilizan secuencia de escape")
+
+tramasConSecuenciaDeEscape = tramasService.localizarTramas(tramasService.detectarSecuenciaEscape, tramas)
+
+print("Las siguientes líneas contienen tramas con secuencia de escape:")
+for linea in tramasConSecuenciaDeEscape:
+    print(linea, ":", tramasService.filtrarSecuenciaEscape(tramas[linea]))
+
+tramasConLongitudIncorrecta = tramasService.localizarTramas(tramasService.validarLongitudTrama, tramas)
+
+print("Las siguientes líneas contienen tramas con longitud incorrecta:")
+for linea in tramasConLongitudIncorrecta:
+    print(linea, ":", tramas[linea])
+
+tramasConChecksumIncorrecto = tramasService.localizarTramas(tramasService.checkSum, tramas)
+
+print("Las siguientes líneas contienen tramas con checksum incorrecto:")
+for linea in tramasConChecksumIncorrecto:
+    print(linea, ":", tramas[linea])
--- TP 1/tramasService.py
+++ TP 1/tramasService.py
@@ -1,6 +1,5 @@
 def separarTramas(text):
     tramas = []
-
     trama = "7E"
 
     for i in range(2, len(text), 2):
@@ -19,14 +18,16 @@ def separarTramas(text):
 def validarLongitudTrama(trama):
     longitudOriginal = len(trama)/2 - 4
     longitud = int(trama[2:6], 16)
-    return longitud == longitudOriginal
+    return longitud != longitudOriginal
 
-def contarTramasCorrectas(tramas):
+
+
+def contarTramasLongitudCorrecta(tramas):
     tramasCorrectas = []
     tramasIncorrectas = []
 
     for trama in tramas:
-        if validarLongitudTrama(trama):
+        if not validarLongitudTrama(trama):
             tramasCorrectas.append(trama)
         else:
             tramasIncorrectas.append(trama)
@@ -37,14 +38,14 @@ def checkSum(trama):
     suma = 0
     for i in range(6, len(trama)-2, 2):
         suma = suma + int(trama[i:i+2], 16)
-    return int(trama[len(trama)-2:len(trama)], 16) == int("FF", 16) - (suma & int("FF", 16))
+    return int(trama[len(trama)-2:len(trama)], 16) != int("FF", 16) - (suma & int("FF", 16))
 
-def verificarTramas(tramas):
+def contarTramasChecksumCorrecto(tramas):
     tramasCorrectas = []
     tramasIncorrectas = []
     i = 0
     for trama in tramas:
-        if checkSum(trama):
+        if not checkSum(trama):
             tramasCorrectas.append(trama)
         else:
             tramasIncorrectas.append(trama)
@@ -52,13 +53,30 @@ def verificarTramas(tramas):
     
     return tramasCorrectas, tramasIncorrectas
 
+def filtrarSecuenciaEscape(trama):
+    trama = trama.replace("7D7E", "7E")
+    return trama
+
+def detectarSecuenciaEscape(trama):
+    for i in range(2, len(trama) - 2, 2):
+            if(trama[i:i+4] == "7D7E"):
+                return True
+    return False
+
 def contarSecuenciasDeEscape(tramas):
     contador = 0
     for trama in tramas:
-        flag = False
-        for i in range(2, len(trama) - 2, 2):
-            if(trama[i:i+4] == "7D7E"):
-                flag = True
-        if(flag):
+        if(detectarSecuenciaEscape(trama)):
             contador += 1
     return contador
+
+def localizarTramas(func, tramas):
+    linea = 0
+    tramasLocalizadas = []
+    for trama in tramas:
+        if(func(trama)):
+            tramasLocalizadas.append(linea)
+        linea += 1
+    return tramasLocalizadas
+
+
